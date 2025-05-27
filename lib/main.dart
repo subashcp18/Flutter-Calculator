b5e26c32f6da8a7cc64cb042865493fed3b55620
Paramasivan02 <paramasivana02@gmail.com>
2025-05-28 00:14:10 +0530

    Theme changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:calculator/buttons.dart';
 import 'package:calculator/helper.dart';
 import 'package:flutter/material.dart';
@@ -8,10 +9,6 @@ import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemUiOverlayStyle(
-    statusBarColor: Color.fromARGB(255, 208, 189, 243),
-    systemNavigationBarColor: Color.fromARGB(255, 208, 189, 243),
-  );
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
@@ -38,6 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   var userAnswer = '';
   bool history = false;
   List historyss = [];
+  final player = AudioPlayer();
   DatabaseHelper db = DatabaseHelper();
 
   final List<String> buttons = [
@@ -74,9 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void fetchAllHistory() async {
     final data = await db.fetchHistory();
     setState(() {
-      print(data);
       historyss = data;
-      print(historyss);
     });
   }
 
@@ -91,18 +87,20 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void dispose() {
     _scrollController.dispose();
+    player.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final screen = MediaQuery.of(context).size;
-    final bgColor = Color.fromARGB(255, 208, 189, 243);
+    // final bgColor = Color.fromARGB(255, 208, 189, 243);
+    const bgColor = Color.fromRGBO(22, 6, 40, 1);
     return Scaffold(
         backgroundColor: bgColor,
         body: SafeArea(
           child: AnnotatedRegion(
-            value: SystemUiOverlayStyle(
+            value: const SystemUiOverlayStyle(
               statusBarColor: bgColor,
               systemNavigationBarColor: bgColor,
             ),
@@ -110,38 +108,39 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 Expanded(
                   flex: 4,
-                  child: InkWell(
-                    splashColor: bgColor,
+                  child: GestureDetector(
                     onTap: () {
-                    setState(() {
-                      history = false;
-                    });
-                  },
+                      setState(() {
+                        history = false;
+                      });
+                    },
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
-                        SizedBox(
+                        const SizedBox(
                           height: 50,
                         ),
                         Container(
-                          padding: EdgeInsets.all(20),
+                          padding: const EdgeInsets.all(20),
                           alignment: Alignment.centerLeft,
                           child: SingleChildScrollView(
                             scrollDirection: Axis.horizontal,
                             controller: _scrollController,
                             child: Text(
                               userQuestion,
-                              style: TextStyle(fontSize: 38),
+                              style: const TextStyle(
+                                  fontSize: 38, color: Colors.white),
                               overflow: TextOverflow.visible,
                             ),
                           ),
                         ),
                         Container(
-                          padding: EdgeInsets.all(20),
+                          padding: const EdgeInsets.all(20),
                           alignment: Alignment.centerRight,
                           child: Text(
                             userAnswer,
-                            style: TextStyle(fontSize: 30),
+                            style: const TextStyle(
+                                fontSize: 30, color: Colors.white),
                           ),
                         ),
                         Row(
@@ -149,14 +148,13 @@ class _MyHomePageState extends State<MyHomePage> {
                             Padding(
                               padding: const EdgeInsets.fromLTRB(
                                   13.0, 0.0, 0.0, 10.0),
-                              child: InkWell(
+                              child: GestureDetector(
                                 onTap: () {
                                   setState(() {
                                     history = !history;
                                   });
-                                  print('tapped');
                                 },
-                                child: Icon(
+                                child: const Icon(
                                   Icons.history_rounded,
                                   size: 33,
                                   color: Colors.white,
@@ -165,9 +163,9 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           ],
                         ),
-                        Divider(
+                        const Divider(
                           height: 3.5,
-                          color: Colors.white,
+                          color: Colors.white38,
                         ),
                       ],
                     ),
@@ -182,10 +180,10 @@ class _MyHomePageState extends State<MyHomePage> {
                         GridView.builder(
                           itemCount: buttons.length,
                           gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
+                              const SliverGridDelegateWithFixedCrossAxisCount(
                                   crossAxisCount: 4),
-                          padding: EdgeInsets.all(0),
-                          physics: NeverScrollableScrollPhysics(),
+                          padding: const EdgeInsets.all(0),
+                          physics: const NeverScrollableScrollPhysics(),
                           itemBuilder: (BuildContext context, int index) {
                             if (index == 0) {
                               return MyButton(
@@ -201,7 +199,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               );
                             } else if (index == 1) {
                               return MyButton(
-                                buttonTapped: () {
+                                buttonTapped: () async {
                                   setState(() {
                                     userQuestion = userQuestion.substring(
                                         0, userQuestion.length - 1);
@@ -212,9 +210,9 @@ class _MyHomePageState extends State<MyHomePage> {
                                 color: Colors.red,
                                 textcolor: Colors.white,
                               );
-                            } else if (index == buttons.length - 1) {
+                            } else if (index == buttons.length - 2) {
                               return MyButton(
-                                buttonTapped: () {
+                                buttonTapped: () async {
                                   setState(() {
                                     equalPressed();
                                     _updateText();
@@ -222,16 +220,29 @@ class _MyHomePageState extends State<MyHomePage> {
                                       db.insertHistory(
                                           userQuestion, userAnswer);
                                     }
-                                    print("success");
                                     fetchAllHistory();
-                                    // historyss.add({
-                                    //   "question": userQuestion,
-                                    //   "answer": userAnswer
-                                    // });
                                   });
                                 },
                                 buttonText: buttons[index],
-                                color: Colors.deepPurple,
+                                color: const Color.fromRGBO(52, 28, 79, 1),
+                                textcolor: Colors.white,
+                              );
+                            } else if (index == buttons.length - 1) {
+                              return MyButton(
+                                buttonTapped: () async {
+                                  await player.play(AssetSource('tap.wav'));
+                                  setState(() {
+                                    equalPressed();
+                                    _updateText();
+                                    if (userQuestion.isNotEmpty) {
+                                      db.insertHistory(
+                                          userQuestion, userAnswer);
+                                    }
+                                    fetchAllHistory();
+                                  });
+                                },
+                                buttonText: buttons[index],
+                                color: const Color.fromRGBO(88, 7, 125, 1),
                                 textcolor: Colors.white,
                               );
                             } else {
@@ -244,11 +255,9 @@ class _MyHomePageState extends State<MyHomePage> {
                                 },
                                 buttonText: buttons[index],
                                 color: isOperator(buttons[index])
-                                    ? Colors.deepPurple
-                                    : Colors.deepPurple[50],
-                                textcolor: isOperator(buttons[index])
-                                    ? Colors.white
-                                    : Colors.deepPurple,
+                                    ? const Color.fromRGBO(88, 7, 125, 1)
+                                    : const Color.fromRGBO(52, 28, 79, 1),
+                                textcolor: Colors.white,
                               );
                             }
                           },
@@ -256,24 +265,26 @@ class _MyHomePageState extends State<MyHomePage> {
                         AnimatedContainer(
                           width: screen.width * 0.72,
                           // height: screen.height - 100,
-                          duration: Duration(milliseconds: 200),
+                          duration: const Duration(milliseconds: 200),
                           curve: Curves.easeInOut,
                           transform: Matrix4.translationValues(
                               history ? 0 : -screen.width, 0, 0),
-                          margin: EdgeInsets.only(right: 0, top: 0, bottom: 0),
+                          margin: const EdgeInsets.only(
+                              right: 0, top: 0, bottom: 0),
                           decoration: BoxDecoration(
-                            color: Colors.deepPurple[180],
+                            color: const Color.fromRGBO(88, 7, 125, 0.2),
+                            // color: Colors.deepPurple[180],
                             borderRadius: BorderRadius.circular(20),
                             border: Border.all(
                               width: 1.0,
-                              color: Colors.white,
+                              color: Colors.white38,
                             ),
                           ),
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(20),
                             child: BackdropFilter(
                               filter:
-                                  ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+                                  ImageFilter.blur(sigmaX: 17.0, sigmaY: 17.0),
                               child: Stack(
                                 children: [
                                   Padding(
@@ -282,7 +293,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                     child: ListView.builder(
                                         itemCount: historyss.length,
                                         shrinkWrap: true,
-                                        padding: EdgeInsets.all(0),
+                                        padding: const EdgeInsets.all(0),
                                         itemBuilder:
                                             (BuildContext context, int index) {
                                           return buildhist(
@@ -294,9 +305,10 @@ class _MyHomePageState extends State<MyHomePage> {
                                   Align(
                                     alignment: Alignment.bottomCenter,
                                     child: GestureDetector(
-                                      onTap: () {
+                                      onTap: () async {
+                                        await player
+                                            .play(AssetSource('tap.wav'));
                                         setState(() {
-                                          // historyss.clear();
                                           db.deleteAllHistory();
                                           fetchAllHistory();
                                         });
@@ -305,7 +317,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                         width: double.infinity,
                                         height: 50,
                                         color: Colors.deepPurple,
-                                        child: Center(
+                                        child: const Center(
                                           child: Text(
                                             "Clear History",
                                             style: TextStyle(
@@ -359,13 +371,9 @@ class _MyHomePageState extends State<MyHomePage> {
     return GestureDetector(
       onTap: () {
         setState(() {
-          print(answer);
           double val = double.parse(answer);
           int roundedValue = val.round();
-          print(roundedValue);
-          // print(val);
           userQuestion += roundedValue.toString();
-          // equalPressed();
         });
       },
       child: Padding(
@@ -374,25 +382,29 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Container(
-              margin: EdgeInsets.only(top: 8.0),
+              margin: const EdgeInsets.only(top: 8.0),
               child: Text(
                 question,
-                style: TextStyle(fontSize: 20, color: Colors.black54),
+                style: const TextStyle(
+                    fontSize: 20,
+                    // color: Colors.black54
+                    color: Colors.white60),
                 overflow: TextOverflow.clip,
                 textAlign: TextAlign.left,
               ),
             ),
             Container(
               width: double.infinity,
-              margin: EdgeInsets.only(bottom: 5.0, top: 5.0),
+              margin: const EdgeInsets.only(bottom: 5.0, top: 5.0),
               child: Text(
-                "= " + answer,
-                style: TextStyle(fontSize: 23, letterSpacing: 0.0),
+                "= $answer",
+                style: const TextStyle(
+                    fontSize: 23, letterSpacing: 0.0, color: Colors.white),
                 overflow: TextOverflow.clip,
                 textAlign: TextAlign.right,
               ),
             ),
-            Divider(
+            const Divider(
               height: 1.0,
               color: Colors.white38,
             ),
